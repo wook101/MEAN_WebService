f0172c4a5f56d848942851a111384ca6069df13d
wook101 <ehddnr1022@gmail.com>
2021-05-18 23:34:33 +0900

    expressServer.js update

--- expressServer.js
+++ expressServer.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-app.use('/',function(req,res){
+app.get('/',function(req,res){
     res.send('hello world');
 });
-
+app.get('/login',function(req,res){
+    res.send('login please');
+})
 app.listen(3000,function(){
     console.log("Running Server");
 });
