0ed6330e3cd189849d2cba00d929d877cea7795a
wook101 <ehddnr1022@gmail.com>
2021-05-26 23:58:51 +0900

    dynamic페이지 생성

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,5 +1,9 @@
 const home = require('../controllers/home');
+const dynamic = require('../controllers/dynamic');
 
 module.exports = function(app){
     app.get('/', home.render);
+
+    app.get('/dynamic', dynamic.render);
+
 };
