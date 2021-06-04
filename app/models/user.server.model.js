ab781b69b549cc10eac142f0c784c9fb7faac088
wook101 <ehddnr1022@gmail.com>
2021-06-04 20:43:28 +0900

    user모델에 생성된 시점을 기록하기 위해 created필드 추가

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -6,7 +6,11 @@ const UserSchema = new Schema({
     lastName: String,
     email: String,
     username: String,
-    password: String
+    password: String,
+    created:{
+        type: Date,
+        default: Date.now
+    }
 },
 {collection:'user'});//collection명 설정 안해주면 defaul값이 users로 지정됨 
 
