43975d8d3355df4a1b606032db23d895c7aa73d0
wook101 <ehddnr1022@gmail.com>
2021-06-04 17:19:07 +0900

    userById 추가

--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -4,4 +4,10 @@ module.exports = function(app){
     app.route('/users')
         .post(users.create)
         .get(users.list);        //모든 document list들을 조회하는 모듈 라우트에 등록
+
+    app.route('/users/:userId')
+        .get(users.read);
+    
+    app.param('userId', users.userByID); //req.user객체를 채우기 위해 먼저 userById실행
+    
 };
