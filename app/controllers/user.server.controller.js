98c9d70d40cd428d27b1e98fb3227e5a8111e333
wook101 <ehddnr1022@gmail.com>
2021-06-04 16:40:54 +0900

    user Collections에 모든 document들 조회기능 추가

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -1,5 +1,6 @@
 const User = require('mongoose').model('User');
 
+//몽고db에 Collection에 user객체 정보 삽입
 exports.create = function(req, res, next){
     const user = new User(req.body);
 
@@ -11,4 +12,16 @@ exports.create = function(req, res, next){
         }
     })
 
+}
+
+//users컬렉션 내에 있는 모든 document들을 조회.
+exports.list = function(req,res,next){
+    User.find({},(err, users)=>{
+        if (err){
+            return next(err);
+        }
+        else{
+            res.status(200).json(users);
+        }
+    });
 }
--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -1,5 +1,7 @@
-const user = require('../controllers/user.server.controller');
+const users = require('../controllers/user.server.controller');
 
 module.exports = function(app){
-    app.route('/user').post(user.create);
+    app.route('/users')
+        .post(users.create)
+        .get(users.list);        //모든 document list들을 조회하는 모듈 라우트에 등록
 };
