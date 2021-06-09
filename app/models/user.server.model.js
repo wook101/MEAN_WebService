b55906835f38b504d1ccd817b1dd444dc622901a
wook101 <ehddnr1022@gmail.com>
2021-06-09 21:46:10 +0900

    mongoose미들웨어 사용하기 pre,post를 이용한 유효성검사,로깅

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -21,7 +21,7 @@ const UserSchema = new Schema({
             function(password){
                 return password.length >= 6;
             },
-            '패스워드가 6자 이상 입력 되지않음.'
+            '패스워드가 6자 이상 입력 되지않음.'  //return값이 false인 경우 다큐먼트를 저장하지 않고 에러 메시지를 콜백에 전달
         ]
     },
     created:{
@@ -52,5 +52,19 @@ UserSchema.virtual('fullName').get(function(){  //fullName필드를 보여주고
     return this.firstName+' '+this.lastName;
 });
 UserSchema.set('toJSON', {getters:true, virtuals:true});
+
+
+//pre미들웨어를 이용한 모델의 유효성 검사
+UserSchema.pre('save',function(next){
+    if (this.username.length>=10){ //유효성 검사 로직 ex)username의 길이가 10이상인 값만 저장 허용
+        next()
+    }else{
+        next(new Error('에러발생'));
+    }
+});
+//post미들웨어를 이용한 로깅
+UserSchema.post('save',function(next){
+    console.log(this.username+"가 저장됬습니다.");
+});
 
 mongoose.model('User', UserSchema);
